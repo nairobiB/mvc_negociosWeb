efed0e41b7f82ff0da4b123303d2e441ab9ee105
Nairobi Bodden <breaniebodden@yahoo.com>
2023-03-07 11:26:35 -0600

    updated to how the teacher has it

--- src/Dao/Mnt/Categoria.php
+++ src/Dao/Mnt/Categoria.php
@@ -1,15 +1,13 @@
 <?php
-
 namespace Controllers\Mnt;
 
 use Controllers\PublicController;
 use Exception;
+use Views\Renderer;
 
-/*
-Cargar la pagina carga los datos de la vista
-*/
 class Categoria extends PublicController
 {
+    private $redirectTo = "index.php?page=Mnt-Categorias";
     private $viewData = array(
         "mode" => "DSP",
         "modedsc" => "",
@@ -20,106 +18,183 @@ class Categoria extends PublicController
         "catest_INA" => "",
         "catnom_error" => "",
         "general_errors" => array(),
-        "has_errors" => false
+        "has_errors" => false,
+        "show_action" => true,
+        "readonly" => false,
     );
-    public function run(): void
-    {
-        try {
-            $this->page_loaded(); //se carga la pagina, si hay un error se manda a la excepcion, y muestra la vista de error
-            if ($this->isPostBack()) {
-                $this->validatePostData();
-            }
-        } catch (Exception $error) {
-
-            error_log(sprintf("Controllers/Mnt/Categoria ERROR: %s", $error->getMessage()));
-            \Utilities\Site::redirectToWithMsg(
-                "index.php?page=Mnt-Categorias",
-                "Algo Inesperado Sucedió. Intente de nuevo."
-            );
-        }
-
-
-
-        /*
-        * 1. Captura de Valores Iniciales QueryParams -> Parámetros de Query ? https://ax.ex.com/index.php?page=abc&mode=UPD&id=1029
-        * 2. Determinar el método POST GET
-        * Todo lo que esta despues del ? son query params
-        * 3. Procesar la Entrada
-        3.1) Si es un POST
-        3.2) Capturar y Calidará datos del formulario
-        3.3) Según el modo realizar la acción
-        3.4) Notificar Error si hay
-        3.5) Redirigir a la Lista
-        4.1) Si es un GET
-        4.2) Obtener valores de la DB si no es INS
-        4.3) Mostrar Valores
-        4. 
-        */
-
-    }
     private $modes = array(
         "DSP" => "Detalle de %s (%s)",
         "INS" => "Nueva Categoría",
         "UPD" => "Editar %s (%s)",
         "DEL" => "Borrar %s (%s)"
     );
+    public function run(): void
+    {
+        try {
+            $this->page_loaded();
+            if ($this->isPostBack()) {
+                $this->validatePostData();
+                if (!$this->viewData["has_errors"]) {
+                    $this->executeAction();
+                }
+            }
+            $this->render();
+        } catch (Exception $error) {
+            error_log(sprintf("Controller/Mnt/Categoria ERROR: %s", $error->getMessage()));
+            \Utilities\Site::redirectToWithMsg(
+                $this->redirectTo,
+                "Algo Inesperado Sucedió. Intente de Nuevo."
+            );
+        }
+        /*
+        1) Captura de Valores Iniciales QueryParams -> Parámetros de Query ? 
+        https://ax.ex.com/index.php?page=abc&mode=UPD&id=1029
+        2) Determinamos el método POST GET
+        3) Procesar la Entrada
+        3.1) Si es un POST
+        3.2) Capturar y Validara datos del formulario
+        3.3) Según el modo realizar la acción solicitada
+        3.4) Notificar Error si hay
+        3.5) Redirigir a la Lista
+        4.1) Si es un GET
+        4.2) Obtener valores de la DB sin no es INS
+        4.3) Mostrar Valores
+        4) Renderizar
+        */
+
+    }
     private function page_loaded()
     {
         if (isset($_GET['mode'])) {
             if (isset($this->modes[$_GET['mode']])) {
-
-                $this->viewData["mode"] = $_GET["mode"];
-
+                $this->viewData["mode"] = $_GET['mode'];
             } else {
-                throw new Exception("Mode Not Available");
-
+                throw new Exception("Mode Not available");
             }
         } else {
             throw new Exception("Mode not defined on Query Params");
-
-
         }
-        if ($this->viewData['mode'] !== "INS") {
-            if (isset($_GET["catid"])) {
-                $this->viewData["catid"];
-
+        if ($this->viewData["mode"] !== "INS") {
+            if (isset($_GET['catid'])) {
+                $this->viewData["catid"] = intval($_GET["catid"]);
             } else {
                 throw new Exception("Id not found on Query Params");
-
-
             }
         }
-
     }
-
     private function validatePostData()
     {
         if (isset($_POST["catnom"])) {
-            //validators tiene varias funciones, se puede ir a la clase y verlas
             if (\Utilities\Validators::IsEmpty($_POST["catnom"])) {
                 $this->viewData["has_errors"] = true;
-                $this->viewData["catnom_error"] = "El nombre no puede ir vacío";
+                $this->viewData["catnom_error"] = "El nombre no puede ir vacío!";
             }
         } else {
             throw new Exception("CatNom not present in form");
         }
         if (isset($_POST["catest"])) {
-
+            if (!in_array($_POST["catest"], array("ACT", "INA"))) {
+                throw new Exception("CatEst incorrect value");
+            }
         } else {
-            throw new Exception("CatEst not present in form");
+            if ($this->viewData["mode"] !== "DEL") {
+                throw new Exception("CatEst not present in form");
+            }
         }
         if (isset($_POST["mode"])) {
-
+            if (!key_exists($_POST["mode"], $this->modes)) {
+                throw new Exception("mode has a bad value");
+            }
+            if ($this->viewData["mode"] !== $_POST["mode"]) {
+                throw new Exception("mode value is different from query");
+            }
         } else {
-            throw new Exception("Mode not present in form");
+            throw new Exception("mode not present in form");
         }
         if (isset($_POST["catid"])) {
-
+            if (($this->viewData["mode"] !== "INS" && intval($_POST["catid"]) <= 0)) {
+                throw new Exception("catId is not Valid");
+            }
+            if ($this->viewData["catid"] !== intval($_POST["catid"])) {
+                throw new Exception("catid value is different from query");
+            }
         } else {
-            throw new Exception("CatId not present in form");
+            throw new Exception("catid not present in form");
+        }
+        $this->viewData["catnom"] = $_POST["catnom"];
+        if ($this->viewData["mode"] !== "DEL") {
+            $this->viewData["catest"] = $_POST["catest"];
         }
     }
-
+    private function executeAction()
+    {
+        switch ($this->viewData["mode"]) {
+            case "INS":
+                $inserted = \Dao\Mnt\Categorias::insert(
+                    $this->viewData["catnom"],
+                    $this->viewData["catest"]
+                );
+                if ($inserted > 0) {
+                    \Utilities\Site::redirectToWithMsg(
+                        $this->redirectTo,
+                        "Categoría Creada Exitosamente"
+                    );
+                }
+                break;
+            case "UPD":
+                $updated = \Dao\Mnt\Categorias::update(
+                    $this->viewData["catnom"],
+                    $this->viewData["catest"],
+                    $this->viewData["catid"]
+                );
+                if ($updated > 0) {
+                    \Utilities\Site::redirectToWithMsg(
+                        $this->redirectTo,
+                        "Categoría Actualizada Exitosamente"
+                    );
+                }
+                break;
+            case "DEL":
+                $deleted = \Dao\Mnt\Categorias::delete(
+                    $this->viewData["catid"]
+                );
+                if ($deleted > 0) {
+                    \Utilities\Site::redirectToWithMsg(
+                        $this->redirectTo,
+                        "Categoría Eliminada Exitosamente"
+                    );
+                }
+                break;
+        }
+    }
+    private function render()
+    {
+        if ($this->viewData["mode"] === "INS") {
+            $this->viewData["modedsc"] = $this->modes["INS"];
+        } else {
+            $tmpCategorias = \Dao\Mnt\Categorias::findById($this->viewData["catid"]);
+            if (!$tmpCategorias) {
+                throw new Exception("Categoria no existe en DB");
+            }
+            //$this->viewData["catnom"] = $tmpCategorias["catnom"];
+            //$this->viewData["catest"] = $tmpCategorias["catest"];
+            \Utilities\ArrUtils::mergeFullArrayTo($tmpCategorias, $this->viewData);
+            $this->viewData["catest_ACT"] = $this->viewData["catest"] === "ACT" ? "selected" : "";
+            $this->viewData["catest_INA"] = $this->viewData["catest"] === "INA" ? "selected" : "";
+            $this->viewData["modedsc"] = sprintf(
+                $this->modes[$this->viewData["mode"]],
+                $this->viewData["catnom"],
+                $this->viewData["catid"]
+            );
+            if (in_array($this->viewData["mode"], array("DSP", "DEL"))) {
+                $this->viewData["readonly"] = "readonly";
+            }
+            if ($this->viewData["mode"] === "DSP") {
+                $this->viewData["show_action"] = false;
+            }
+        }
+        Renderer::render("mnt/categoria", $this->viewData);
+    }
 }
 
 ?>
--- src/Dao/Mnt/Categorias.php
+++ src/Dao/Mnt/Categorias.php
@@ -1,60 +1,81 @@
 <?php
-
 namespace Dao\Mnt;
 
 use Dao\Table;
 
 /*
-CREATE TABLE `nw202101`.`categorias` (
 `catid` BIGINT(8) NOT NULL AUTO_INCREMENT,
 `catnom` VARCHAR(45) NULL,
 `catest` CHAR(3) NULL DEFAULT 'ACT',
-PRIMARY KEY (`catid`));
 */
+/**
+ * Undocumented class
+ */
 class Categorias extends Table
 {
     /**
-     * Crea un nuevo registro de categoria
+     * Crea un nuevo registro de categoria.
      *
-     * @param string $catnom
-     * @param string $catest
-     * @return void
+     * @param string $catnom description
+     * @param string $catest description
+     *
+     * @return int
      */
     public static function insert(string $catnom, string $catest = "ACT"): int
     {
-        $sqlstr = "INSERT INTO categorias (catnom, catest) values(?, ?);";
-        $rowinserted = self::executeNonQuery(
+        $sqlstr = "INSERT INTO categorias (catnom, catest) values(:catnom, :catest);";
+        $rowsInserted = self::executeNonQuery(
+            $sqlstr,
+            array("catnom" => $catnom, "catest" => $catest)
+        );
+        return $rowsInserted;
+    }
+    public static function update(
+        string $catnom,
+        string $catest,
+        int $catid
+    )
+    {
+        $sqlstr = "UPDATE categorias set catnom = :catnom, catest = :catest where catid=:catid;";
+        $rowsUpdated = self::executeNonQuery(
             $sqlstr,
             array(
                 "catnom" => $catnom,
-                "catest" => $catest
+                "catest" => $catest,
+                "catid" => $catid
             )
         );
-
-        return $rowinserted;
-
+        return $rowsUpdated;
     }
-    public static function update()
+    public static function delete(int $catid)
     {
-
-
-    }
-    public static function delete()
-    {
-
-
+        $sqlstr = "DELETE from categorias where catid=:catid;";
+        $rowsDeleted = self::executeNonQuery(
+            $sqlstr,
+            array(
+                "catid" => $catid
+            )
+        );
+        return $rowsDeleted;
     }
     public static function findAll()
     {
-        $sqlstr = "SELECT * FROM categorias;";
+        $sqlstr = "SELECT * from categorias;";
         return self::obtenerRegistros($sqlstr, array());
-
     }
-    public static function findById()
+    public static function findByFilter()
     {
 
-
+    }
+    public static function findById(int $catid)
+    {
+        $sqlstr = "SELECT * from categorias where catid = :catid;";
+        $row = self::obtenerUnRegistro(
+            $sqlstr,
+            array(
+                "catid" => $catid
+            )
+        );
+        return $row;
     }
 }
-
-?>
